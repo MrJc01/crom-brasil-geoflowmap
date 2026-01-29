afb57052bb0041a7371eea8e905c07a51745fa80
MrJ <78050688+MrJc01@users.noreply.github.com>
2026-01-29 04:43:27 -0300

    1.0.4

--- vite.config.standalone.ts
+++ vite.config.standalone.ts
@@ -4,6 +4,11 @@ import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [react()],
+    define: {
+        'process.env': {
+            NODE_ENV: JSON.stringify('production')
+        }
+    },
     build: {
         outDir: 'dist-standalone',
         lib: {
